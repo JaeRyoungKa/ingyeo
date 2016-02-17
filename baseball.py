14ea8eed06025120f88069b02b73108b90d8bafb
JaeRyoungKa <jaeryoung.ka@gmail.com>
2016-02-17 20:35:32 +0900

    random baseball game #2
    random baseball game with python. press h to hit, press n not to hit.
    flyout function has not yet been implemented; so even if one get a
    fly-out, no base movement is applied.

--- baseball.py
+++ baseball.py
@@ -2,8 +2,8 @@ import random
 
 # chance of result
 hr = 1
-out = 9
-flyout = 5
+out = 10
+flyout = 6
 foul = 4
 h1 = 9
 h2 = 3
@@ -45,40 +45,78 @@ def score():
 
 
 def process():
+    global inn
+    global runnerlist
+    global sub
+    global team1
+    global team2
     while(inn<=9):
         out = strike = ball = 0
-        global runnerlist
-        global sub
-        global inn
-        global team1
-        global team2
         runnerlist = []
         while(not(out>=3)):
-            result = batting()
-            if(result=="hr"):
-                score()
-                runnerlist = []
-            if(result=="out"):
-                out += 1
-            if(result=="h1"):
-                run(1)
-            if(result=="h2"):
-                run(2)
-            if(result=="h3"):
-                run(3)
-            if(result=="hr"):
-                run(4)
-            if(result=="foul"):
-                if(strike==2):
-                    strike += 0
+            temp = input('입력? (h for hitting, n for not hitting)')
+            if temp == 'h':
+                if random.randrange(0,100)<= 40:
+                    global result
+                    result = batting()
+                    if(result=="hr"):
+                        score()
+                        strike = ball = 0
+                    if(result=="out"):
+                        out += 1
+                        strike = ball = 0
+                    if(result=="h1"):
+                        run(1)
+                        strike = ball = 0
+                    if(result=="h2"):
+                        run(2)
+                        strike = ball = 0
+                    if(result=="h3"):
+                        run(3)
+                        strike = ball = 0
+                    if(result=="hr"):
+                        if sub:
+                            team1 += len(runnerlist)
+                        if not sub:
+                            team2 += len(runnerlist)
+                        runnerlist = []
+                        strike = ball = 0
+                    if(result=="foul"):
+                        if(strike==2):
+                            strike += 0
+                        else:
+                            strike += 1
+                    if(result=="flyout"):
+                        out +=1
+                        strike = ball = 0
                 else:
                     strike += 1
-            if(result=="flyout"):
-                out +=1
-            print(inn,"회",runnerlist,out,"아웃",team1,":",team2,result)
+                    result="헛스윙"
+                    if strike == 3:
+                        out +=1
+                        strike = ball = 0
+                        result="스트라이크 아웃"
+
+            if temp == 'n':
+                if random.randrange(0,100)<=60:
+                    ball += 1
+                    result='볼'
+                    if ball == 4:
+                        run(1)
+                        strike = ball = 0
+                        result="볼넷"
+                else:
+                    strike += 1
+                    result="스트라이크"
+                    if strike == 3:
+                        out +=1
+                        strike = ball = 0
+                        result='스트라이크 아웃'
+            print(result,":::",ball,"-",strike,":::",inn,"회",sub,runnerlist,out,"아웃",team1,":",team2)
         if not sub:
             inn += 1
         sub = not sub
+        print("===========이닝교체=============")
 
 
 def run(much):
@@ -87,14 +125,11 @@ def run(much):
     for runner in runnerlist:
         temp.append(runner+much)
     temp.append(much)
-    for runner in temp:
-        if(runner>=4):
-            score()
-            temp.remove(runner)
-    for runner in temp:
-        if(runner>=4):
-            score()
-            temp.remove(runner)
+    for i in range(0,4):
+        for runner in temp:
+            if(runner >= 4):
+                score()
+                temp.remove(runner)
     runnerlist = temp
     runnerlist.sort()
 
